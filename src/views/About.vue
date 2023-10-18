dd2aa8de84be4f660d6a899f74899d2a696d3d0c
HoaiSon - RIKAI MIND <hoaison.nguyen@rikai.technology>
2023-10-18 17:59:37 +0700

    setup view router

---
+++ src/views/About.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>About</h1>
+    <p>This is the about page</p>
+    <CounterButton />
+</template>
+
+<script setup>
+    import CounterButton from '../components/CounterButton.vue';
+</script>
