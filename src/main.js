2bd24e7ad77954d1a38f0036594c9d219d493d35
HoaiSon - RIKAI MIND <hoaison.nguyen@rikai.technology>
2023-10-20 17:56:40 +0700

    building weather app

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from '@/App.vue'
 import router from '@/plugins/router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import '@/style.css'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(VueAxios, axios).mount('#app')
--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,8 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
+    //view
     {path: '', name: 'home', component:() => import('@/views/Home.vue')},
     {path: '/about', name: 'about', component:() => import('@/views/About.vue')},
+    {path: '/weather', name: 'weather', component:() => import('@/views/Weather.vue')}
+
+    //weather app
+    
 ]
 
 const router = createRouter({
